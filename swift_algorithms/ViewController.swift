510a44734bdd623c4a35f204185832570217ecda
Alexander Murphy <amurphy345@gmail.com>
2018-08-14 19:14:11 -0600

    Initial Commit

---
+++ swift_algorithms/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift_algorithms
+//
+//  Created by Alexander Murphy on 8/14/18.
+//  Copyright © 2018 Alexander Murphy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
