5ea729b9f73127594b3338f8481f6149779c8cd7
Alexander Murphy <amurphy345@gmail.com>
2018-08-30 22:09:38 -0600

    adding file

--- swift_algorithms/DataStructurePresenter.swift
+++ swift_algorithms/DataStructurePresenter.swift
@@ -4,65 +4,6 @@ protocol DataStructurePresenterActionDispatching: AnyObject {
     func dispatch(_ action: DataStructurePresenter.Action)
 }
 
-final class DataStructureSectionFactory {
-    
-    struct IdentifiableSectionController {
-        let seciton: TableSectionController
-        let identifiers: [UUID: DataStructurePresenter.Action]
-   }
-    
-    struct IdentifiableRowProperties {
-        let properties: [BasicTableRowController.Properties]
-        let identifiers: [UUID: DataStructurePresenter.Action]
-    }
-    
-    func makeSectionController(title: String,
-                               footerText: String?,
-                               dataStructures: [DataStructure]) -> IdentifiableSectionController {
-        
-        let sectionController = BasicTableSectionController()
-        let identifiableProperties = makeIdentifiableProperties(for: dataStructures)
-        
-        sectionController.rows = identifiableProperties.properties.map(BasicTableRowController.map)
-        sectionController.sectionTitle = title
-        sectionController.footerText = footerText
-        
-        return IdentifiableSectionController(seciton: sectionController,
-                                             identifiers: identifiableProperties.identifiers)
-    }
-    
-    
-    private func makeIdentifiableProperties(for dataStructures: [DataStructure]) -> IdentifiableRowProperties {
-        
-        var actionLookup: [UUID: DataStructurePresenter.Action] = [:]
-        
-        let identifiableProperties: [BasicTableRowController.Properties] = dataStructures.map { dataStructure in
-            let identifier = UUID()
-            let properties = BasicTableRowController.Properties(title: dataStructure.title,
-                                                                subtitle: dataStructure.subtitle,
-                                                                showsDisclosure: true,
-                                                                identifier: identifier)
-            actionLookup[identifier] = .selectedDataStructure(dataStructure)
-            return properties
-        }
-        
-        return IdentifiableRowProperties(properties: identifiableProperties,
-                                         identifiers: actionLookup)
-    }
-    
-    func makeAllDataStructureSections() -> [IdentifiableSectionController] {
-        let allDataStructureCategories = DataStructure.Category.allCases
-        
-        return allDataStructureCategories.map { category in
-            let section = makeSectionController(title: category.title,
-                                                footerText: category.footerText,
-                                                dataStructures: category.dataStructures)
-            return section
-        }
-    }
-    
-}
-
 final class DataStructurePresenter {
     
     enum Action {
