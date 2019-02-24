2b4d018909d04bfb6df32f34c8704fb106511be2
Dumitru Vilceanu <doruvil@yahoo.com>
2019-02-24 19:34:11 +0100

    Extract `GettingStartedAlgorithm` enum from previously overloaded `Algorithm` enum into standalone file.

---
+++ swift_algorithms/Models/GettingStartedAlgorithm.swift
@@ -0,0 +1,61 @@
+import Foundation
+
+enum GettingStartedAlgorithm: CaseIterable, SortingAlgorithmable {
+    case stack
+    case queue
+    case insertionSort
+    case binarySearch
+    case binarySearchTree
+    case mergeSort
+    case boyerMoore
+}
+
+extension GettingStartedAlgorithm: Algorithm {
+    
+    var category: AlgorithmCategory {
+        return .gettingStarted
+    }
+
+    var title: String {
+        switch self {            
+        case .stack:
+            return "Stack"
+        case .queue:
+            return "Queue"
+        case .insertionSort:
+            return "Insertion Sort"
+        case .binarySearch:
+            return "Binary Search"
+        case .binarySearchTree:
+            return "Binary Search Tree"
+        case .mergeSort:
+            return "Merge Sort"
+        case .boyerMoore:
+            return "Boyer Moore"
+        }
+    }
+    
+    var subtitle: String? {
+        return nil
+    }
+    
+    var resourceUrl: String {
+        switch self {
+            
+        case .stack:
+            return "https://raw.githubusercontent.com/raywenderlich/swift-algorithm-club/master/Stack/README.markdown"
+        case .queue:
+            return "https://raw.githubusercontent.com/raywenderlich/swift-algorithm-club/master/Queue/README.markdown"
+        case .insertionSort:
+            return "https://raw.githubusercontent.com/raywenderlich/swift-algorithm-club/master/Insertion%20Sort/README.markdown"
+        case .binarySearch:
+            return "https://raw.githubusercontent.com/raywenderlich/swift-algorithm-club/master/Binary%20Search/README.markdown"
+        case .binarySearchTree:
+            return "https://raw.githubusercontent.com/raywenderlich/swift-algorithm-club/master/Binary%20Search%20Tree/README.markdown"
+        case .mergeSort:
+            return "https://raw.githubusercontent.com/raywenderlich/swift-algorithm-club/master/Merge%20Sort/README.markdown"
+        case .boyerMoore:
+            return "https://raw.githubusercontent.com/raywenderlich/swift-algorithm-club/master/Boyer-Moore-Horspool/README.markdown"
+        }
+    }
+}
