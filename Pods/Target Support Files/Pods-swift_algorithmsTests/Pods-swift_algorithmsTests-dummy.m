a71d85050f03d0c42d18d52b4e94570aeab4e5b5
Alexander Murphy <amurphy345@gmail.com>
2019-04-21 20:21:53 -0600

    remove pods

--- Pods/Target Support Files/Pods-swift_algorithmsTests/Pods-swift_algorithmsTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_swift_algorithmsTests : NSObject
-@end
-@implementation PodsDummy_Pods_swift_algorithmsTests
-@end
--- Pods/Target Support Files/Pods-swift_algorithmsUITests/Pods-swift_algorithmsUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_swift_algorithmsUITests : NSObject
-@end
-@implementation PodsDummy_Pods_swift_algorithmsUITests
-@end
