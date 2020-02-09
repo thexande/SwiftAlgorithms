ad335d625474fded3a1e876507270a5296ffcd8d
Alexander Murphy <amurphy345@gmail.com>
2020-02-09 16:38:41 -0700

    more cleanup

--- swift_algorithms/Application/AppDelegate.swift
+++ swift_algorithms/Application/AppDelegate.swift
@@ -2,10 +2,8 @@ import UIKit
 
 @UIApplicationMain
 final class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
     private var coordinator: Coordinating?
-    
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
