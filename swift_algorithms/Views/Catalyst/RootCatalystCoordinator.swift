53c861551b206e2d23c78658dbfbaa887abc743a
Alexander Murphy <amurphy345@gmail.com>
2020-02-09 19:26:51 -0700

    adding search controller

--- swift_algorithms/Views/Catalyst/RootCatalystCoordinator.swift
+++ swift_algorithms/Views/Catalyst/RootCatalystCoordinator.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Anchorage
 
 @available(iOS 13.0, *)
 final class RootCatalystCoordinator: Coordinating {
@@ -10,7 +11,6 @@ final class RootCatalystCoordinator: Coordinating {
     private let sideMenuView = SideMenuTableViewController()
     private let categoryNavigationView = UINavigationController()
     private let about = AboutViewController()
-
     
     init() {
         mainMenuSplitView.primaryBackgroundStyle = .sidebar
@@ -31,10 +31,54 @@ final class RootCatalystCoordinator: Coordinating {
         root = mainMenuSplitView
         
         sideMenuPresenter.delegate = self
+    
+        let view = UINavigationController(rootViewController: CatalystSearchViewController())
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.root?.present(view, animated: false, completion: nil)
+        }
     }
 }
 
-// MARK: - MainCatalystPresenterDelegate
+final class CatalystSearchViewController: UIViewController {
+    
+    private let algorithmSearchResultsController = SearchResultsTableViewController(style: .grouped)
+    private let algorithmSearchResultsPresenter = AlgorithmSearchPresenter()
+    let algorithmSearchController = UISearchController(searchResultsController: nil)
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        algorithmSearchResultsPresenter.deliver = { [weak algorithmSearchResultsController] properties in
+            algorithmSearchResultsController?.properties = properties
+        }
+        
+        algorithmSearchResultsPresenter.makeSearchableAlgorithmProperties()
+//        algorithmSearchResultsPresenter.dispatcher = self
+        
+        algorithmSearchResultsController.dispatcher = algorithmSearchResultsPresenter
+        
+        algorithmSearchController.searchResultsUpdater = algorithmSearchResultsController
+        algorithmSearchController.obscuresBackgroundDuringPresentation = false
+        algorithmSearchController.searchBar.placeholder = "Search Algorithms"
+        
+        navigationItem.searchController = algorithmSearchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        
+        algorithmSearchResultsController.willMove(toParent: self)
+        view.addSubview(algorithmSearchResultsController.view)
+        algorithmSearchResultsController.view.edgeAnchors == view.edgeAnchors
+        algorithmSearchResultsController.didMove(toParent: self)
+        
+        algorithmSearchResultsController.tableView.contentInset = .init(top: 56, left: 0, bottom: 0, right: 0)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        algorithmSearchController.isActive = true
+    }
+}
 
 @available(iOS 13.0, *)
 extension RootCatalystCoordinator: MainCatalystPresenterDelegate {
